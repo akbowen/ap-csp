fb8c68aa9d1452534e03646dd6837fcf54ae3a71
Andrew Bowen <andrewb2004@outlook.com>
2020-02-14 13:12:55 -0500

    Update ball-game.js

--- ball-game.js
+++ ball-game.js
@@ -4,12 +4,19 @@ var canvas = document.getElementById("myCanvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var ballRadius = 100;
+var ballRadius = 75;
 var x = canvas.width/2;
 var y = canvas.height-300;
 
 var dx = 2;
 var dy = -2;
+
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth) / 2;
+
+var rightPressed = false;
+var leftPressed = false;
 
 function drawBall() {
   ctx.beginPath();
@@ -19,11 +26,25 @@ function drawBall() {
   ctx.closePath();
 }
 
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "blue";
+  ctx.fill();
+  ctx.closePath();
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
   x += dx;
   y += dy;
+  if(rightPressed) {
+    paddleX += 7;
+  } else if(leftPressed) {
+    paddleX -= 7;
+  }
   if(x + dx > canvas.width-ballRadius) {
     dx = -dx;
   }
@@ -35,6 +56,24 @@ function draw() {
   }
   if (y + dy > canvas.height-ballRadius) {
     dy = -dy;
+  }
+}
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if(e.key === "Right" || e.key === "ArrowRight") {
+    rightPressed = true;
+  } else if(e.key === "Left" || e.key === "ArrowLeft") {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if(e.key === "Right" || e.key === "ArrowRight") {
+    rightPressed = false;
+  } else if(e.key === "Left" || e.key === "ArrowLeft") {
+    leftPressed = false;
   }
 }
 
