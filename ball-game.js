f3adb3f3c0832f1945e1f0763b98b937737a6a10
Andrew Bowen <andrewb2004@outlook.com>
2020-02-20 11:12:33 -0500

    Update ball-game.js

--- ball-game.js
+++ ball-game.js
@@ -1,15 +1,15 @@
 var canvas = document.getElementById("myCanvas");
-  var ctx = canvas.getContext("2d");
+    var ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var ballRadius = 75;
+var ballRadius = 25;
 var x = canvas.width/2;
 var y = canvas.height-300;
 
-var dx = 2;
-var dy = -2;
+var dx = 1;
+var dy = -1;
 
 var paddleHeight = 10;
 var paddleWidth = 75;
@@ -20,7 +20,7 @@ var leftPressed = false;
 
 function drawBall() {
   ctx.beginPath();
-  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+  ctx.arc(x,y, ballRadius, 0, Math.PI*2);
   ctx.fillStyle = "green";
   ctx.fill();
   ctx.closePath();
@@ -34,47 +34,55 @@ function drawPaddle() {
   ctx.closePath();
 }
 
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+function draw(){
+  ctx.clearRect(0,0, canvas.width, canvas.height);
   drawBall();
   drawPaddle();
   x += dx;
   y += dy;
   if(rightPressed) {
     paddleX += 7;
+    if (paddleX + paddleWidth > canvas.width){
+      paddleX = canvas.width - paddleWidth;
+    }
   } else if(leftPressed) {
     paddleX -= 7;
+    if (paddleX < 0) {
+      paddleX = 0;
+    }
   }
-  if(x + dx > canvas.width-ballRadius) {
-    dx = -dx;
-  }
-  if(x + dx < ballRadius) {
+  if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
     dx = -dx;
   }
   if(y + dy < ballRadius) {
     dy = -dy;
-  }
-  if (y + dy > canvas.height-ballRadius) {
-    dy = -dy;
+  } else if (y + dy > canvas.height-ballRadius) {
+    if(x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy;
+    } else {
+      alert("GAME OVER");
+      document.location.reload();
+      clearInterval(interval);
+    }
   }
 }
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
 function keyDownHandler(e) {
-  if(e.key === "Right" || e.key === "ArrowRight") {
+  if(e.key == "Right" || e.key == "ArrowRight") {
     rightPressed = true;
-  } else if(e.key === "Left" || e.key === "ArrowLeft") {
+  } else if(e.key == "Left" || e.key == "ArrowLeft") {
     leftPressed = true;
-  }
+}
 }
 
 function keyUpHandler(e) {
-  if(e.key === "Right" || e.key === "ArrowRight") {
+  if(e.key == "Right" || e.key == "ArrowRight") {
     rightPressed = false;
-  } else if(e.key === "Left" || e.key === "ArrowLeft") {
+  } else if(e.key == "Left" || e.key == "ArrowLeft") {
     leftPressed = false;
   }
 }
 
-setInterval(draw, 10);
+var interval = setInterval(draw, 10);
