1dec484aaaaf2605227c6f8739a08748c7525cba
Andrew Bowen <andrewb2004@outlook.com>
2020-02-25 09:40:03 -0500

    Update ball-game.js

--- ball-game.js
+++ ball-game.js
@@ -5,8 +5,8 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var ballRadius = 25;
-var x = canvas.width/2;
-var y = canvas.height-300;
+var x = canvas.width / 2;
+var y = canvas.height - 300;
 
 var dx = 1;
 var dy = -1;
@@ -17,6 +17,25 @@ var paddleX = (canvas.width-paddleWidth) / 2;
 
 var rightPressed = false;
 var leftPressed = false;
+
+var brickRowCount = 3;
+var brickColumnCount = 7;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+var bricks = [];
+for(var c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for(var r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = {x: 0, y: 0, status : 1};
+  }
+}
+
+var brickX = (c*(brickWidth + brickPadding)) + brickOffsetLeft;
+var brickY = (r*(brickHeight + brickPadding)) + brickOffsetTop;
 
 function drawBall() {
   ctx.beginPath();
@@ -34,10 +53,30 @@ function drawPaddle() {
   ctx.closePath();
 }
 
+function drawBricks(){
+  for(var c = 0; c < brickColumnCount; c++) {
+    for(var r = 0; r < brickRowCount; r++) {
+      if(bricks[c][r].status == 1) {
+        var brickX = (c*(brickWidth + brickPadding)) + brickOffsetLeft;
+        var brickY = (r*(brickHeight+brickPadding)) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
+    }
+  }
+}
+
 function draw(){
   ctx.clearRect(0,0, canvas.width, canvas.height);
+  drawBricks();
   drawBall();
   drawPaddle();
+  collisionDetection();
   x += dx;
   y += dy;
   if(rightPressed) {
@@ -82,6 +121,20 @@ function keyUpHandler(e) {
     rightPressed = false;
   } else if(e.key == "Left" || e.key == "ArrowLeft") {
     leftPressed = false;
+  }
+}
+
+function collisionDetection() {
+  for(var c = 0; c < brickColumnCount; c++) {
+    for(var r = 0; r < brickRowCount; r++) {
+      var b = bricks[c][r];
+      if(b.status == 1){
+        if(x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+          dy = -dy;
+          b.status = 0;
+        }
+      }
+    }
   }
 }
 
