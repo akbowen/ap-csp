06e05bd79b4b26935e02f709e3ae05e41d6087fd
Andrew Bowen <andrewb2004@outlook.com>
2020-02-27 10:48:35 -0500

    Update ball-game.js

--- ball-game.js
+++ ball-game.js
@@ -1,30 +1,30 @@
 var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
+var ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var ballRadius = 25;
+var ballRadius = 10;
 var x = canvas.width / 2;
 var y = canvas.height - 300;
 
-var dx = 2;
-var dy = -2;
+var dx = 1;
+var dy = -1;
 
 var paddleHeight = 10;
 var paddleWidth = 75;
-var paddleX = (canvas.width-paddleWidth) / 2;
+var paddleX = (canvas.width - paddleWidth) / 2;
 
 var rightPressed = false;
 var leftPressed = false;
 
-var brickRowCount = 3;
-var brickColumnCount = 7;
+var brickRowCount = 5;
+var brickColumnCount = 9;
 var brickWidth = 75;
 var brickHeight = 20;
-var brickPadding = 10;
-var brickOffsetTop = 30;
-var brickOffsetLeft = 30;
+var brickPadding = 0;
+var brickOffsetTop = 50;
+var brickOffsetLeft = 0;
 
 var bricks = [];
 for(var c = 0; c < brickColumnCount; c++) {
@@ -44,7 +44,7 @@ var lives = 3;
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x,y, ballRadius, 0, Math.PI*2);
-  ctx.fillStyle = "green";
+  ctx.fillStyle = "gray";
   ctx.fill();
   ctx.closePath();
 }
@@ -52,7 +52,7 @@ function drawBall() {
 function drawPaddle() {
   ctx.beginPath();
   ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-  ctx.fillStyle = "blue";
+  ctx.fillStyle = "red";
   ctx.fill();
   ctx.closePath();
 }
@@ -67,7 +67,12 @@ function drawBricks(){
         bricks[c][r].y = brickY;
         ctx.beginPath();
         ctx.rect(brickX, brickY, brickWidth, brickHeight);
-        ctx.fillStyle = "#0095DD";
+        if(r === 0) ctx.fillStyle = 'red';
+        if(r === 1) ctx.fillStyle = 'orange';
+        if(r === 2) ctx.fillStyle = 'yellow';
+        if(r === 3) ctx.fillStyle = 'green';
+        if(r === 4) ctx.fillStyle = 'blue';
+        // if(r === 5) ctx.fillStyle = 'purple';
         ctx.fill();
         ctx.closePath();
       }
@@ -75,8 +80,17 @@ function drawBricks(){
   }
 }
 
+function drawBackground(){
+  ctx.beginPath();
+  ctx.rect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "black";
+  ctx.fill();
+  ctx.closePath();
+}
+
 function draw(){
   ctx.clearRect(0,0, canvas.width, canvas.height);
+  drawBackground();
   drawBricks();
   drawBall();
   drawPaddle();
@@ -109,8 +123,7 @@ function draw(){
       if(!lives) {
         alert("GAME OVER");
         document.location.reload();
-      }
-      else {
+      } else {
         x = canvas.width / 2;
         y = canvas.height - 30;
         dx = 2;
@@ -148,12 +161,12 @@ function collisionDetection() {
         if(x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
           dy = -dy;
           b.status = 0;
+          // add acceleration in the future
+          // if(bricks[r] === 4) dx = 1.2; dy = 1.2;
           score++;
           if(score == brickRowCount*brickColumnCount) {
             alert("YOU WIN, CONGRATULATIONS!");
-            
             document.location.reload();
-            
           }
         }
       }
@@ -169,7 +182,7 @@ function drawScore() {
 
 function drawLives() {
   ctx.font = "16px Arial";
-  ctx.fillStyle = "0095DD";
+  ctx.fillStyle = "#0095DD";
   ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
 }
 
